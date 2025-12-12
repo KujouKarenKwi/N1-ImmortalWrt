4b341953ffe1fb11a435fe7f082d243002198bce
KujouKarenKwi <115976160+KujouKarenKwi@users.noreply.github.com>
2025-12-12 19:03:24 +0800

    1

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -6,11 +6,12 @@ patch -p1 -f < $(dirname "$0")/luci.patch
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
-git clone https://github.com/rufengsuixing/luci-app-adguardhome --depth=1 clone/adguardhome
+git clone https://github.com/stevenjoezhang/luci-app-adguardhome --depth=1 clone/luci-app-adguardhome
 
 # Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
-cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall clone/adguardhome/luci-app-adguardhome feeds/luci/applications/
+cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -rf clone/luci-app-adguardhome feeds/luci/applications/
 
 # Clean packages
 rm -rf clone
